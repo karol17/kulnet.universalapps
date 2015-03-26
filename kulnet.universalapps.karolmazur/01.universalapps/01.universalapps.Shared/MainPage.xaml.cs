08229c02f1c53a051fc402d3964da16d7017b664
karol17 <karol.mazur17@hotmail.com>
2015-03-26 20:53:14 +0100

    Pierwsza aplikacja uniwersalna

---
+++ kulnet.universalapps.karolmazur/01.universalapps/01.universalapps.Shared/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace _01.universalapps
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+           
+            var ComboBox = Combo;
+            ComboBox.Items.Add("Informacja");
+            ComboBox.Items.Add("Biuro");
+            ComboBox.Items.Add("PR");
+            ComboBox.SelectedIndex = 1;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+           
+        }
+    }
+}
